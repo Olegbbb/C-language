7950e0920c3ac0d84b83cc453a9bb5aac3996a3d
Oleg <apricot-ob@yandex.ru>
2022-10-02 21:37:56 +0300

    Condition

---
+++ 05Example_ConditionElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string? username = Console.ReadLine();
+
+if(username?.ToLower() == "олег")
+{
+    Console.WriteLine("Ебать ты красавчик");
+}
+else
+{
+    Console.Write("Съебал отсюда, ");
+    Console.WriteLine(username);
+}
