1f6bf56dbc9e2e8137b28c86e42d4e29e1c34ed0
Oleg <apricot-ob@yandex.ru>
2022-10-06 14:48:35 +0300

    MethodArray

---
+++ 10Example_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {15,92,13,49,18,51,56,27,18};
+
+int n = array.Length;
+int find = 18;
+
+for(int index = 0; index < n; index++  )
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+}
