b38c487e69d6eb979c33efd139157cf44b1642f6
Oleg <apricot-ob@yandex.ru>
2022-10-06 16:32:56 +0300

    MethodLibrary

---
+++ 11Example_MethodLibrary/Program.cs
@@ -0,0 +1,40 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++)
+    {
+        Console.WriteLine(col[position]);
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int face = -1;
+    for(int index = 0; index < count; index++)
+    {
+        if(collection[index]==find)
+        {
+            face = index;
+            break;
+        }
+    }
+    return face;
+}
+
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
