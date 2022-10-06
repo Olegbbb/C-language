2ae7805c8b53d2381310f40743edf52ba44dd9a5
Oleg <apricot-ob@yandex.ru>
2022-10-06 14:09:39 +0300

    Method

---
+++ 09Example_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 32, 43, 11, 65, 98, 23, 48, 77, 35 };
+
+
+int max = Max(Max(array[0], array[1], array[2]), Max( array[3], array[4], array[5]),Max( array[6], array[7], array[8]));
+
+Console.WriteLine(max);
